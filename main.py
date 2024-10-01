f6e2ae4621b27d5f7b19956497d9014fa474700e
Dehaz <jorgedehaz147@gmail.com>
2024-10-01 09:43:07 +0200

    dehazmartinez

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import clientes
 from venPrincipal import*
 import sys
 import var
 import eventos
+import conexion
+import styles
 
 class Main(QtWidgets.QMainWindow):
 
@@ -11,12 +14,21 @@ class Main(QtWidgets.QMainWindow):
         var.ui.setupUi(self)
 
         '''
-        
-        
         zona de eventos del menubar
         '''
         var.ui.actionSalir.triggered.connect(eventos.Eventos.mensajeSalir)
 
+        '''
+        eventos de botones
+        '''
+
+        var.ui.btnGrabarcli.clicked.connect(clientes.Clientes.altaCliente)
+
+        '''
+        eventos de cajas de texto
+        '''
+
+        var.ui.txtDnicli.editingFinished.connect(lambda:clientes.Clientes.checkDNI(var.ui.txtDnicli.text()))
 
 
 if __name__ == '__main__':
